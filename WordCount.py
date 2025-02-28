8e1505aee37b4b5e5d455b0dfe663dad27ffd8ba
BMW060 <brennan.m.wood@gmail.com>
2025-02-28 22:23:17 +0000

    Working on character count

--- WordCount.py
+++ WordCount.py
@@ -1,6 +1,6 @@
 #WordCount.py
-#Name:
-#Date:
+#Name: Brennan Wood
+#Date: 
 #Assignment:
 
 def main():
@@ -14,24 +14,18 @@ def main():
 
   for line in textFile:
     lineCount = lineCount +1
-    words = line.split(" ")
+    words = line.split()
     for w in words:
       wordCount = wordCount + 1
+      words = words.split()
+      for w in words:
+        letterCount = letterCount + 1
 
-  for c in line:
-    characterCount = characterCount + 1
-
-  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # borrowed this from Caesar Cipher to count actual letters
-  for c in line:
-    c = c.upper()
-    if c in alpha:
-      letterCount = letterCount + 1
 
 
   print("Lines: ",lineCount)
   print("Words: ",wordCount)
   print("Characters (including spaces): ",letterCount)
-  print("Characters (not including spaces): ",letterCount)
 
 
 if __name__ == '__main__':
