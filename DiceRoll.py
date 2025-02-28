bf00a157421afcedb80e681404a8854b34b9140d
BMW060 <brennan.m.wood@gmail.com>
2025-02-28 14:55:18 +0000

    DiceRoll randome section

--- DiceRoll.py
+++ DiceRoll.py
@@ -1,17 +1,29 @@
 #DiceRoll.py
-#Name:
-#Date:
-#Assignment:
+#Name: Brennan Wood
+#Date:2/28/25
+#Assign/ment: Lab 6 DiceRoll
+
 import random
 
 def main():
   #Create an empty list with possible roll values
-  rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
+  rolls = [0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
+  
+  for r in range(100):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    dice = dice1 + dice2
+    rolls[dice-2] = rolls[dice-2] + 1
+  
   
   #find the sum total of the two dice
   
   #print statictics for dice rolls
+  spot = 2
+  for count in rolls:
+    print(spot,":",count)
+    spot = spot + 1
 
 
 if __name__ == '__main__':
